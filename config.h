d3b0ad2a1e4b5958c1c6ea723fb8f39bd85842e6
Wei Lok <49401808+Wayloque@users.noreply.github.com>
2020-10-08 17:45:18 +0800

    Change border color and remove unused colors

--- config.h
+++ config.h
@@ -16,15 +16,12 @@ static const char *alttrayname      = "tray";    /* Polybar tray instance name *
 static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray[]        = "#444444";
+static const char bor_color[]       = "#98971a";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { NULL, 	    NULL,      col_gray },
+	[SchemeSel]  = { NULL, 	    NULL,      bor_color },
 };
 
 /* tagging */
