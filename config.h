cc8c6ad75fa3d16a2a0b0227164970bf66612ae4
Wong Wei Lok <dev@wongweilok.com>
2021-03-22 17:01:02 +0800

    Remove unused code

--- config.h
+++ config.h
@@ -50,7 +50,6 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
-#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
 
 static const Layout layouts[] = {
