f123fb54050bc30fb223fa7d98073cd32346fbe8
Wong Wei Lok <wongweilok.personal@zohomail.com>
2021-06-07 22:18:29 +0800

    Patches update and minor changes

--- config.h
+++ config.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
@@ -42,7 +42,6 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
-#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
 
 static const Layout layouts[] = {
@@ -86,10 +85,6 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_o,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,              		XK_z,      incrgaps,       {.i = +5 } },
-	{ MODKEY|ShiftMask,             XK_z,      incrgaps,       {.i = -5 } },
-	{ MODKEY,              		XK_a,      togglegaps,     {0} },
-	{ MODKEY|ShiftMask,    		XK_d,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_space,  zoom,           {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
@@ -107,6 +102,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,			XK_a,	   togglegaps,	   {0} },
+	{ MODKEY|ShiftMask,		XK_d,	   defaultgaps,	   {0} },
+	{ MODKEY,			XK_z,	   incrgaps,	   {.i = +5 } },
+	{ MODKEY|ShiftMask,		XK_z,	   incrgaps,	   {.i = -5 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -128,6 +127,7 @@ static Key keys[] = {
 
 	{ MODKEY|ShiftMask,		XK_x,	   spawn,	   SHCMD("powerOption") },
 	{ MODKEY|ShiftMask,		XK_k,	   spawn,	   SHCMD("fcitx") },
+	{ MODKEY|ShiftMask,		XK_p,	   spawn,	   SHCMD("passmenu") },
 	{ MODKEY,			XK_grave,  spawn,	   SHCMD("dmenuunicode") },
 	{ 0,				XK_Print,  spawn,	   SHCMD("ssOption") },
 
